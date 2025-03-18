20cade6e05ddbcbaaebda0cd93c1c2910304c8e3
Tony Lea <tony@devdojo.com>
2025-03-18 17:10:06 -0400

    Simplest echo form

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,19 @@
 
 use App\Events\TestMessage;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Broadcast;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test-websocket', function () {
-    return view('test-websocket');
-});
-
 Route::get('/trigger-message', function () {
-    \Log::info('Triggering TestMessage event');
-    event(new TestMessage());
-    return 'Message sent!';
+    Broadcast::on('test-message')
+        ->as('test.message')
+        ->with(['message' => 'Hello World'])
+        ->send();
+    // \Log::info('Triggering TestMessage event');
+    // event(new TestMessage());
+    // return 'Message sent!';
+    return 'message sent';
 });
